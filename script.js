6ab4112a00bff8234795652397ca2404c588cc3e
sackie0813 <jsjsone0813@gmail.com>
2024-08-07 02:07:04 -0400

    Created very simple div creation loop

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const container = document.querySelector("container");
-makeRow(16);
+const container = document.getElementById("container");
 
 function makeRow (rowNum) {
     for (let i = 0; i < rowNum; i++) {
-    let newCell = document.createElement("div");
+    let row = document.createElement("div");
+    container.appendChild(row).className = "gridRows";
     }
-    container.appendChild(newCell);
 }
+
+makeRow(5);
