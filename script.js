d944399e30402be371ca66772f9d37bdb386b02a
sackie0813 <jsjsone0813@gmail.com>
2024-08-07 01:41:17 -0400

    Created index, script, styles

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.querySelector("container");
+makeRow(16);
+
+function makeRow (rowNum) {
+    for (let i = 0; i < rowNum; i++) {
+    let newCell = document.createElement("div");
+    }
+    container.appendChild(newCell);
+}
