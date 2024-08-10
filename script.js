10b0da02ba2e167784e6774107ce59af23774c56
sackie0813 <jsjsone0813@gmail.com>
2024-08-09 21:28:44 -0400

    Created a grid clear button and reformatted code

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const container = document.getElementById("container");
-let cellSize = 0;
 
 // Makes a grid
 function makeGrid (size) {
@@ -7,7 +6,7 @@ function makeGrid (size) {
         const gridTile = document.createElement("div");
         gridTile.classList.add("tile");
         container.appendChild(gridTile);
-        gridTile.style.width =(480/size) + "px";
+        gridTile.style.width =(480/size) + "px"; //480px Since container borders add up to be 20
         gridTile.style.height = (480/size) + "px";
         // Handles changing color to blue when mouse is over
         gridTile.addEventListener ("mouseover" , function() {
@@ -17,24 +16,35 @@ function makeGrid (size) {
     }
 }
 
+// Handles dimensions of the tiles, removes all tiles and replaces with new ones
 const sizeButton = document.createElement("button");
-    sizeButton.classList.add("sizeButton");
-    sizeButton.textContent = "Change Dimensions";
-    document.body.appendChild(sizeButton);
-    sizeButton.addEventListener ("click" , function() {
-        let item = document.getElementsByClassName("tile") 
-        while (item[0]) {
-            item[0].parentNode.removeChild(item[0]);
-        }
-        const size = prompt("Enter desired dimensions between 1-100");
-        if (size < 1 || size > 100 || size === null) {
-            alert("You have entered an invalid number");
-            makeGrid(1);
-        } else {
-            makeGrid(size);
-        }
-    });
+sizeButton.classList.add("sizeButton");
+sizeButton.textContent = "Change Dimensions";
+document.body.appendChild(sizeButton);
+sizeButton.addEventListener ("click" , function() {
+    let item = document.getElementsByClassName("tile") 
+    while (item[0]) { // While existing tiles exist, remove the first until none exist
+        item[0].parentNode.removeChild(item[0]);
+    }
+    const size = prompt("Enter desired dimensions between 1-100");
+    if (size < 1 || size > 100 || size === null) {
+        alert("You have entered an invalid number");
+        makeGrid(1);
+    } else {
+        makeGrid(size);
+    }
+});
 
+const resetButton = document.createElement("button");
+resetButton.classList.add("reset");
+resetButton.textContent = "Reset";
+resetButton.addEventListener("click", function(){
+    const item = document.getElementsByClassName("tile");
+    for (let i = 0; i < item.length; i++) {
+        item[i].style.backgroundColor = "";
+    }
+});
+document.body.appendChild(resetButton);
 
 
 makeGrid(16);
